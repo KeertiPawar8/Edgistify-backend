8e28b08315766f09390d8fd0da662da2d68fa81b
KeertiPawar8 <keertipawar8@gmail.com>
2025-02-12 18:12:46 +0530

    assignment

--- routes/product.routes.js
+++ routes/product.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const productRouter = express.Router();
 const { ProductModel } = require("../models/product.model");
 const { CartModel } = require("../models/cart.model");
-const {OrderModel}= require("../models/order.model")
+const { OrderModel } = require("../models/order.model");
 const { authenticate } = require("../middlewares/authenticate.middleware");
 
 productRouter.get("/getProducts", async (req, res) => {
@@ -38,14 +38,35 @@ productRouter.post("/addToCart", authenticate, async (req, res) => {
 productRouter.get("/getCartProducts", authenticate, async (req, res) => {
   try {
     const { userID } = req.body;
-    const cartData = await CartModel.find({userID});
-    return res.status(200).json({cartData});
+    const cartData = await CartModel.aggregate([
+      { $match: { userID } },
+      {
+        $lookup: {
+          from: "products",
+          localField: "productID",
+          foreignField: "productID",
+          as: "productDetails",
+        },
+      },
+      { $unwind: "$productDetails" },
+      {
+        $project: {
+          productID: 1,
+          name: "$productDetails.name",
+          image: "$productDetails.image",
+          price: "$productDetails.price",
+          quantity: 1,
+        },
+      },
+    ]);
+
+    return res.status(200).json({ cartData });
   } catch (error) {
     return res.status(500).json({ error: "Internal Server Error" });
   }
 });
 
-productRouter.delete("/removeFromCart", authenticate,async (req, res) => {
+productRouter.delete("/removeFromCart", authenticate, async (req, res) => {
   const { productID, userID } = req.body;
 
   if (!productID || !userID) {
@@ -61,7 +82,7 @@ productRouter.delete("/removeFromCart", authenticate,async (req, res) => {
   res.status(200).json({ message: "Product removed from cart successfully!" });
 });
 
-productRouter.put("/increase-quantity", authenticate,async (req, res) => {
+productRouter.put("/increase-quantity", authenticate, async (req, res) => {
   try {
     const { productID, userID } = req.body;
 
@@ -86,7 +107,7 @@ productRouter.put("/increase-quantity", authenticate,async (req, res) => {
   }
 });
 
-productRouter.put("/decrease-quantity", authenticate,async (req, res) => {
+productRouter.put("/decrease-quantity", authenticate, async (req, res) => {
   try {
     const { productID, userID } = req.body;
 
@@ -118,7 +139,7 @@ productRouter.put("/decrease-quantity", authenticate,async (req, res) => {
   }
 });
 
-productRouter.get("/total-price",authenticate, async (req, res) => {
+productRouter.get("/total-price", authenticate, async (req, res) => {
   try {
     const { userID } = req.body;
     const cart = await CartModel.aggregate([
@@ -150,61 +171,70 @@ productRouter.get("/total-price",authenticate, async (req, res) => {
   }
 });
 
-productRouter.post("/place-order",authenticate, async (req, res) => {
-    try {
-      const { userID ,shippingAddress} = req.body;
+productRouter.post("/place-order", authenticate, async (req, res) => {
+  try {
+    const { userID, shippingAddress } = req.body;
     if (!shippingAddress) {
-        return res.status(400).json({ message: "Shipping address is required." });
-      }
-    const cart = await CartModel.aggregate([
-        { $match: { userID } },
-        {
-          $lookup: {
-            from: "products",
-            localField: "productID",
-            foreignField: "productID",
-            as: "productDetails",
-          },
-        },
-        {$unwind:"$productDetails"},
-        {
-          $project: {
-            productID: 1,
-            quantity: 1,
-            price: "$productDetails.price",
-            name:"$productDetails.name",
-            stock:"$productDetails.stock"
-          },
-        },
-      ]);
-      const insufficientStock = cart.filter(item => item.quantity > item.stock);
-      if (insufficientStock.length > 0) {
-        const productNames = insufficientStock.map(item => item.name).join(", ");
-        return res.status(400).json({ error: `Insufficient stock for products: ${productNames}` });
-      }
-      const totalPrice = cart.reduce((sum, item) => sum + item.price * item.quantity, 0);
-
-      const orderData = {
-        userID,
-        products: cart,
-        totalPrice,
-        shippingAddress,
-      };
-      const newOrder = new OrderModel(orderData);
-      await newOrder.save();
-
-      for (const item of cart) {
-        await ProductModel.updateOne(
-          { productID: item.productID },
-          { $inc: { stock: -item.quantity } }
-        );
-      }
-      await CartModel.deleteMany({ userID });
-      return res.status(201).json({ message: "Order placed successfully", order: newOrder });
-    } catch (error) {
-      res.status(500).json({ error: "Internal server error" });
+      return res.status(400).json({ message: "Shipping address is required." });
     }
-  });
+    const cart = await CartModel.aggregate([
+      { $match: { userID } },
+      {
+        $lookup: {
+          from: "products",
+          localField: "productID",
+          foreignField: "productID",
+          as: "productDetails",
+        },
+      },
+      { $unwind: "$productDetails" },
+      {
+        $project: {
+          productID: 1,
+          quantity: 1,
+          price: "$productDetails.price",
+          name: "$productDetails.name",
+          stock: "$productDetails.stock",
+        },
+      },
+    ]);
+    const insufficientStock = cart.filter((item) => item.quantity > item.stock);
+    if (insufficientStock.length > 0) {
+      const productNames = insufficientStock
+        .map((item) => item.name)
+        .join(", ");
+      return res
+        .status(400)
+        .json({ error: `Insufficient stock for products: ${productNames}` });
+    }
+    const totalPrice = cart.reduce(
+      (sum, item) => sum + item.price * item.quantity,
+      0
+    );
+
+    const orderData = {
+      userID,
+      products: cart,
+      totalPrice,
+      shippingAddress,
+    };
+    const newOrder = new OrderModel(orderData);
+    await newOrder.save();
+
+    for (const item of cart) {
+      await ProductModel.updateOne(
+        { productID: item.productID },
+        { $inc: { stock: -item.quantity } }
+      );
+    }
+    await CartModel.deleteMany({ userID });
+    return res
+      .status(201)
+      .json({ message: "Order placed successfully", order: newOrder });
+  } catch (error) {
+    res.status(500).json({ error: "Internal server error" });
+  }
+});
 
 module.exports = {
   productRouter,
